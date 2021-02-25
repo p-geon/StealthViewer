5c0aab53be32fd14581b6f20b6854b15f336517d
p-geon <nagano@nlab.ci.i.u-tokyo.ac.jp>
2021-02-25 12:25:13 +0900

    update

---
+++ image_loader.py
@@ -0,0 +1,31 @@
+import numpy as np
+from PIL import ImageStat, Image
+
+def check_img(func):
+	def wrapper(*args, **kwargs):
+		print('--image information-')
+		img = args[0]
+		print(f"shape: {img.shape}")
+		print(f"dtype: {type(img)}")
+		print(f"max: {img.max()}")
+		print(f"min: {img.min()}")
+		print(f"ave: {np.mean(img)}")
+		plt.figure()
+		plt.hist(img, bins=20)
+		plt.show()
+		print('------')
+		func(*args, **kwargs)
+	return wrapper
+
+def main():
+	path = "./src/fff.png"
+	img = Image.open(path)
+	stat = ImageStat.Stat(img)
+	print('pixel num:', stat.count) # Number of Pixel
+	print('pixel sum:', stat.sum)   # Sum of Pixel Value
+	print('pixel mean:', stat.mean) # Mean of Pixel Values
+	print('pixel variance:', stat.var) #variance of all Pixels
+	print('pixel stddev:', stat.stddev) #standerd deviation
+
+if(__name__=="__main__"):
+	main()
